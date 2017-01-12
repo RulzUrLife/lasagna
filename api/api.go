8e64173ffc614514ef31e5e6c0f8ea57ea841163
Maxime Vidori <maxime.vidori@adyen.com>
2017-01-12 17:28:33 +0100

    Small refactoring, better use of golang

--- api/api.go
+++ api/api.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/RulzUrLife/lasagna/common"
 	"net/http"
 	"path"
@@ -19,22 +18,26 @@ type Templates struct {
 	XML  *template.Template
 }
 
-type Endpoint struct {
-	Name string
-	List struct {
-		Method ListMethod
-		*Templates
-	}
-	Get struct {
-		Method GetMethod
-		*Templates
-	}
+type List struct {
+	Method ListMethod
+	*Templates
 }
 
-var base = path.Join("templates", "base.html")
-var errTplt = &Templates{
-	template.Must(template.ParseFiles(base, path.Join("templates", "error.html"))),
-	nil,
+type Get struct {
+	Name   string
+	Method GetMethod
+	*Templates
+}
+
+var errTplt = templates("error.html")
+
+func templates(paths ...string) *Templates {
+	base := path.Join("templates", "base.html")
+	paths = append([]string{"templates"}, paths...)
+	return &Templates{
+		template.Must(template.ParseFiles(base, path.Join(paths...))),
+		nil,
+	}
 }
 
 type ResponseWriter interface {
@@ -43,9 +46,7 @@ type ResponseWriter interface {
 }
 
 type HTMLResponseWriter struct {
-	*Endpoint
 	*template.Template
-	ErrorTemplate *template.Template
 }
 
 func (rw *HTMLResponseWriter) Render(w http.ResponseWriter, data interface{}) {
@@ -54,12 +55,10 @@ func (rw *HTMLResponseWriter) Render(w http.ResponseWriter, data interface{}) {
 
 func (rw *HTMLResponseWriter) Error(w http.ResponseWriter, err *common.HTTPError) {
 	w.WriteHeader(err.Code)
-	rw.ErrorTemplate.Execute(w, err)
+	errTplt.HTML.Execute(w, err)
 }
 
-type JSONResponseWriter struct {
-	*Endpoint
-}
+type JSONResponseWriter struct{}
 
 func (rw *JSONResponseWriter) Render(w http.ResponseWriter, data interface{}) {
 	json.NewEncoder(w).Encode(data)
@@ -70,7 +69,7 @@ func (rw *JSONResponseWriter) Error(w http.ResponseWriter, err *common.HTTPError
 	json.NewEncoder(w).Encode(err)
 }
 
-func (e *Endpoint) parseHeaders(w http.ResponseWriter, r *http.Request, t *Templates) ResponseWriter {
+func parseHeaders(w http.ResponseWriter, r *http.Request, t *Templates) ResponseWriter {
 	accepts := r.Header["Accept"]
 	for _, media := range strings.Split(accepts[0], ",") {
 		media = strings.Split(media, ";")[0]
@@ -78,7 +77,7 @@ func (e *Endpoint) parseHeaders(w http.ResponseWriter, r *http.Request, t *Templ
 		case "text/html":
 			common.Trace.Printf("HTML rendering")
 			w.Header().Set("Content-Type", "text/html")
-			return &HTMLResponseWriter{e, t.HTML, errTplt.HTML}
+			return &HTMLResponseWriter{t.HTML}
 		case "application/xhtml+xml":
 			common.Trace.Printf("XML rendering")
 			w.Header().Set("Content-Type", "application/xhtml+xml")
@@ -87,16 +86,16 @@ func (e *Endpoint) parseHeaders(w http.ResponseWriter, r *http.Request, t *Templ
 	}
 	common.Trace.Printf("JSON rendering")
 	w.Header().Set("Content-Type", "application/json")
-	return &JSONResponseWriter{e}
+	return &JSONResponseWriter{}
 }
 
-func (e *Endpoint) get(w http.ResponseWriter, r *http.Request) {
-	rw := e.parseHeaders(w, r, e.Get.Templates)
-	if url := r.URL.Path[len(e.Name)+1:]; url == "" {
-		http.Redirect(w, r, e.Name, http.StatusSeeOther)
+func (g *Get) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	rw := parseHeaders(w, r, g.Templates)
+	if url := r.URL.Path[len(g.Name)+1:]; url == "" {
+		http.Redirect(w, r, g.Name, http.StatusSeeOther)
 	} else if i, err := strconv.Atoi(url); err != nil {
 		rw.Error(w, common.NewHTTPError("400 Bad Request", http.StatusBadRequest))
-	} else if data, err := e.Get.Method(i); err != nil {
+	} else if data, err := g.Method(i); err != nil {
 		rw.Error(w, err)
 	} else {
 		common.Trace.Printf("%q", data)
@@ -104,9 +103,9 @@ func (e *Endpoint) get(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (e *Endpoint) list(w http.ResponseWriter, r *http.Request) {
-	rw := e.parseHeaders(w, r, e.List.Templates)
-	if data, err := e.List.Method(); err != nil {
+func (l *List) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	rw := parseHeaders(w, r, l.Templates)
+	if data, err := l.Method(); err != nil {
 		rw.Error(w, err)
 	} else {
 		common.Trace.Printf("%q", data)
@@ -122,21 +121,9 @@ func (mux *ServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (mux *ServeMux) NewEndpoint(name string, list ListMethod, get GetMethod) {
-	endpoint := &Endpoint{Name: name}
-	dir := path.Join("templates", name)
-
-	endpoint.List.Method = list
-	endpoint.List.Templates = &Templates{
-		template.Must(template.ParseFiles(base, path.Join(dir, "list.html"))),
-		nil,
-	}
-
-	endpoint.Get.Method = get
-	endpoint.Get.Templates = &Templates{
-		template.Must(template.ParseFiles(base, path.Join(dir, "get.html"))),
-		nil,
-	}
-
-	mux.HandleFunc(name, endpoint.list)
-	mux.HandleFunc(fmt.Sprintf("%s/", name), endpoint.get)
+	mux.Handle(name, &List{list, templates(name, "list.html")})
+	mux.Handle(
+		strings.Join([]string{name, "/"}, ""),
+		&Get{name, get, templates(name, "get.html")},
+	)
 }
