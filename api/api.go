e8c4462c48906ca56a5a812dad141633e6ed0afd
Maxime Vidori <maxime.vidori@adyen.com>
2017-01-23 14:52:46 +0100

    Drop support for JSON

--- api/api.go
+++ api/api.go
@@ -1,19 +1,12 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/RulzUrLife/lasagna/common"
 	"net/http"
 	"strconv"
-	"strings"
 	"text/template"
 )
-
-type Templates struct {
-	HTML *template.Template
-	XML  *template.Template
-}
 
 type Create struct {
 	*BaseHandler
@@ -29,116 +22,39 @@ type Get struct {
 
 type BaseHandler struct {
 	Name string
-	*Templates
+	*template.Template
 	common.Endpoint
-}
-
-func (bh *BaseHandler) GetTemplates() *Templates {
-	return bh.Templates
 }
 
 func NewHandler(name string, html string, ressource common.Endpoint) *BaseHandler {
 	return &BaseHandler{common.Url(name), templates(name, html), ressource}
 }
 
-type Handler interface {
-	ServeHTTP(w ResponseWriter, r *http.Request)
-	GetTemplates() *Templates
-}
-
-func Handle(h Handler) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		rw := parseHeaders(w, r, h.GetTemplates())
-		h.ServeHTTP(rw, r)
-	}
-}
-
 var errTplt = templates("error.html")
 
-func templates(paths ...string) *Templates {
+func templates(paths ...string) *template.Template {
 	base := common.Path(common.Config.Assets.Templates, "base.html")
 	p := common.Path(append([]string{common.Config.Assets.Templates}, paths...)...)
 	funcs := template.FuncMap{
 		"div": div, "slice": slice, "url": url,
 	}
 	common.Info.Printf("Register template %s", p)
-	return &Templates{
-		template.Must(template.New("base.html").Funcs(funcs).ParseFiles(base, p)),
-		nil,
-	}
+	return template.Must(template.New("base.html").Funcs(funcs).ParseFiles(base, p))
 }
 
-type ResponseWriter interface {
-	Write(data interface{}) error
-	Writer() http.ResponseWriter
-}
-
-type HTMLResponseWriter struct {
-	w http.ResponseWriter
-	*template.Template
-}
-
-func (rw *HTMLResponseWriter) Write(data interface{}) error {
-	if err, ok := data.(*common.HTTPError); ok {
-		rw.w.WriteHeader(err.Code)
-		return errTplt.HTML.Execute(rw.w, data)
-	} else {
-		return rw.Template.Execute(rw.w, data)
-	}
-}
-
-func (rw *HTMLResponseWriter) Writer() http.ResponseWriter {
-	return rw.w
-}
-
-type JSONResponseWriter struct {
-	w http.ResponseWriter
-}
-
-func (rw *JSONResponseWriter) Write(data interface{}) error {
-	if err, ok := data.(*common.HTTPError); ok {
-		rw.w.WriteHeader(err.Code)
-	}
-	return json.NewEncoder(rw.w).Encode(data)
-}
-
-func (rw *JSONResponseWriter) Writer() http.ResponseWriter {
-	return rw.w
-}
-
-func parseHeaders(w http.ResponseWriter, r *http.Request, t *Templates) ResponseWriter {
-	accepts := r.Header["Accept"]
-	for _, media := range strings.Split(accepts[0], ",") {
-		media = strings.Split(media, ";")[0]
-		switch media {
-		case "text/html":
-			common.Trace.Printf("HTML rendering")
-			w.Header().Set("Content-Type", "text/html")
-			return &HTMLResponseWriter{w, t.HTML}
-		case "application/xhtml+xml":
-			common.Trace.Printf("XML rendering")
-			w.Header().Set("Content-Type", "application/xhtml+xml")
-			return nil
-		}
-	}
-	common.Trace.Printf("JSON rendering")
-	w.Header().Set("Content-Type", "application/json")
-	return &JSONResponseWriter{w}
-}
-
-func (g *Get) ServeHTTP(rw ResponseWriter, r *http.Request) {
+func (g *Get) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	del := func(id int) {
 		if err := g.Delete(id); err != nil {
-			rw.Write(err)
+			errTplt.Execute(w, err)
 		} else {
-			rw.Writer().WriteHeader(http.StatusNoContent)
+			w.WriteHeader(http.StatusNoContent)
 		}
 	}
 	get := func(id int) {
 		if data, err := g.Get(id); err != nil {
-			rw.Write(err)
-		} else if err := rw.Write(data); err != nil {
+			errTplt.Execute(w, err)
+		} else if err := g.Template.Execute(w, data); err != nil {
 			common.Error.Printf("Rendering failed: %s", err)
 		} else {
 			common.Trace.Printf("%q", data)
@@ -147,10 +63,10 @@ func (g *Get) ServeHTTP(rw ResponseWriter, r *http.Request) {
 
 	if url := r.URL.Path[len(g.Name)+1:]; url == "" {
 		// trailing / redirect to base url
-		http.Redirect(rw.Writer(), r, g.Name, http.StatusSeeOther)
+		http.Redirect(w, r, g.Name, http.StatusSeeOther)
 	} else if id, err := strconv.Atoi(url); err != nil {
 		// non parsable parameter
-		rw.Write(common.New400Error(
+		errTplt.Execute(w, common.New400Error(
 			fmt.Sprintf("Invalid %s id '%s'", g.Name, url),
 		))
 	} else {
@@ -161,62 +77,28 @@ func (g *Get) ServeHTTP(rw ResponseWriter, r *http.Request) {
 		case http.MethodGet:
 			get(id)
 		default:
-			rw.Write(common.New404Error("Method does not exist on this endpoint"))
+			errTplt.Execute(w, common.New404Error("Method does not exist on this endpoint"))
 		}
 	}
 }
 
-func (l *List) ParsePost(rw ResponseWriter, r *http.Request) (common.Endpoint, *common.HTTPError) {
-	var err error
-	var e = l.New()
-
+func (l *List) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	switch rw.(type) {
-	case *HTMLResponseWriter:
-		if r.ParseForm(); err != nil {
-			return nil, common.New400Error(err.Error())
-		} else if e, err = e.ValidateForm(r.Form); err != nil {
-			return nil, common.New400Error(err.Error())
-		}
-		common.Trace.Printf("%q", r.Form)
-	case *JSONResponseWriter:
-		//
-		decoder := json.NewDecoder(r.Body)
-
-		if err = decoder.Decode(e); err != nil {
-			return nil, common.New400Error(err.Error())
-		} else if err = e.Validate(); err != nil {
-			return nil, common.New400Error(err.Error())
-		}
-	}
-	if err = e.Save(); err != nil {
-		common.Error.Printf("%s", err)
-		return nil, common.New500Error(err.Error())
-	} else {
-		common.Trace.Printf("%q", e)
-		return e, nil
-	}
-}
-
-func (l *List) ServeHTTP(rw ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		if resource, err := l.ParsePost(rw, r); err != nil {
-			rw.Write(err)
+		if err := r.ParseForm(); err != nil {
+			errTplt.Execute(w, common.New400Error(err.Error()))
+		} else if e, err := l.ValidateForm(r.Form); err != nil {
+			errTplt.Execute(w, common.New400Error(err.Error()))
+		} else if err := e.Save(); err != nil {
+			common.Error.Printf("%s", err)
+			errTplt.Execute(w, common.New500Error(err.Error()))
 		} else {
-			switch rw.(type) {
-			case *HTMLResponseWriter:
-				http.Redirect(
-					rw.Writer(), r, common.Url(l.Name, resource.Hash()),
-					http.StatusSeeOther,
-				)
-			default:
-				rw.Write(resource)
-			}
+			http.Redirect(w, r, common.Url(l.Name, e.Hash()), http.StatusSeeOther)
 		}
 	} else {
 		if data, err := l.List(); err != nil {
-			rw.Write(err)
-		} else if err := rw.Write(data); err != nil {
+			errTplt.Execute(w, err)
+		} else if err := l.Template.Execute(w, data); err != nil {
 			common.Error.Printf("Rendering failed: %s", err)
 		} else {
 			common.Trace.Printf("%q", data)
@@ -224,27 +106,22 @@ func (l *List) ServeHTTP(rw ResponseWriter, r *http.Request) {
 	}
 }
 
-func (c *Create) ServeHTTP(rw ResponseWriter, r *http.Request) {
-	switch rw.(type) {
-	case *HTMLResponseWriter:
-		rw.Write(struct{}{})
-	default:
-		// endpoint not available for something else than html
-		rw.Write(common.New404Error("Endpoint only exists for mimetype: text/html"))
-	}
+func (c *Create) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c.Template.Execute(w, struct{}{})
 }
 
 type serveMux struct{ *http.ServeMux }
 
 func (mux *serveMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	common.Info.Printf("%s %s", r.Method, r.URL.Path)
 	mux.ServeMux.ServeHTTP(w, r)
 }
 
 func (mux *serveMux) NewEndpoint(name string, ressource common.Endpoint) {
-	mux.HandleFunc(common.Url(name), Handle(&List{NewHandler(name, "list.html", ressource)}))
-	mux.HandleFunc(common.Url(name, ""), Handle(&Get{NewHandler(name, "get.html", ressource)}))
-	mux.HandleFunc(common.Url(name, "new"), Handle(&Create{NewHandler(name, "create.html", ressource)}))
+	mux.Handle(common.Url(name), &List{NewHandler(name, "list.html", ressource)})
+	mux.Handle(common.Url(name, ""), &Get{NewHandler(name, "get.html", ressource)})
+	mux.Handle(common.Url(name, "new"), &Create{NewHandler(name, "create.html", ressource)})
 }
 
 var Mux = &serveMux{http.NewServeMux()}
