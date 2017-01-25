4532b31690707df6734be975fe88c480df2f007e
Maxime Vidori <maxime.vidori@adyen.com>
2017-01-25 17:02:33 +0100

    Simplify basic handler

--- api/api.go
+++ api/api.go
@@ -1,34 +1,11 @@
 package api
 
 import (
-	"fmt"
 	"github.com/RulzUrLife/lasagna/common"
 	"net/http"
 	"strconv"
 	"text/template"
 )
-
-type Create struct {
-	*BaseHandler
-}
-
-type List struct {
-	*BaseHandler
-}
-
-type Get struct {
-	*BaseHandler
-}
-
-type BaseHandler struct {
-	Name string
-	*template.Template
-	common.Endpoint
-}
-
-func NewHandler(name string, html string, ressource common.Endpoint) *BaseHandler {
-	return &BaseHandler{common.Url(name), templates(name, html), ressource}
-}
 
 var errTplt = templates("error.html")
 
@@ -42,74 +19,6 @@ func templates(paths ...string) *template.Template {
 	return template.Must(template.New("base.html").Funcs(funcs).ParseFiles(base, p))
 }
 
-func (g *Get) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	del := func(id int) {
-		if err := g.Delete(id); err != nil {
-			errTplt.Execute(w, err)
-		} else {
-			w.WriteHeader(http.StatusNoContent)
-		}
-	}
-	get := func(id int) {
-		if data, err := g.Get(id); err != nil {
-			errTplt.Execute(w, err)
-		} else if err := g.Template.Execute(w, data); err != nil {
-			common.Error.Printf("Rendering failed: %s", err)
-		} else {
-			common.Trace.Printf("%q", data)
-		}
-	}
-
-	if url := r.URL.Path[len(g.Name)+1:]; url == "" {
-		// trailing / redirect to base url
-		http.Redirect(w, r, g.Name, http.StatusSeeOther)
-	} else if id, err := strconv.Atoi(url); err != nil {
-		// non parsable parameter
-		errTplt.Execute(w, common.New400Error(
-			fmt.Sprintf("Invalid %s id '%s'", g.Name, url),
-		))
-	} else {
-		// switch through http methods, invoke the correct one
-		switch r.Method {
-		case http.MethodDelete:
-			del(id)
-		case http.MethodGet:
-			get(id)
-		default:
-			errTplt.Execute(w, common.New404Error("Method does not exist on this endpoint"))
-		}
-	}
-}
-
-func (l *List) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	if r.Method == http.MethodPost {
-		if err := r.ParseForm(); err != nil {
-			errTplt.Execute(w, common.New400Error(err.Error()))
-		} else if e, err := l.ValidateForm(r.Form); err != nil {
-			errTplt.Execute(w, common.New400Error(err.Error()))
-		} else if err := e.Save(); err != nil {
-			common.Error.Printf("%s", err)
-			errTplt.Execute(w, common.New500Error(err.Error()))
-		} else {
-			http.Redirect(w, r, common.Url(l.Name, e.Hash()), http.StatusSeeOther)
-		}
-	} else {
-		if data, err := l.List(); err != nil {
-			errTplt.Execute(w, err)
-		} else if err := l.Template.Execute(w, data); err != nil {
-			common.Error.Printf("Rendering failed: %s", err)
-		} else {
-			common.Trace.Printf("%q", data)
-		}
-	}
-}
-
-func (c *Create) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c.Template.Execute(w, struct{}{})
-}
-
 type serveMux struct{ *http.ServeMux }
 
 func (mux *serveMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -118,10 +27,59 @@ func (mux *serveMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	mux.ServeMux.ServeHTTP(w, r)
 }
 
-func (mux *serveMux) NewEndpoint(name string, ressource common.Endpoint) {
-	mux.Handle(common.Url(name), &List{NewHandler(name, "list.html", ressource)})
-	mux.Handle(common.Url(name, ""), &Get{NewHandler(name, "get.html", ressource)})
-	mux.Handle(common.Url(name, "new"), &Create{NewHandler(name, "create.html", ressource)})
+func (mux *serveMux) NewEndpoint(name string, resource common.Endpoint) {
+	var handler http.HandlerFunc
+	var listTemplate = templates(name, "list.html")
+	var getTemplate = templates(name, "get.html")
+	var newTemplate = templates(name, "new.html")
+
+	handler = func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			if r.URL.Path == "new" {
+				defer r.Body.Close()
+				if err := r.ParseForm(); err != nil {
+					errTplt.Execute(w, common.New400Error(err.Error()))
+				} else if e, err := resource.ValidateForm(r.Form); err != nil {
+					errTplt.Execute(w, common.New400Error(err.Error()))
+				} else if err := e.Save(); err != nil {
+					common.Error.Printf("%s", err)
+					errTplt.Execute(w, common.New500Error(err.Error()))
+				} else {
+					http.Redirect(w, r, common.Url(name, e.Hash()), http.StatusSeeOther)
+				}
+				break
+			}
+			fallthrough
+		case http.MethodGet:
+			switch url := r.URL.Path; url {
+			case "":
+				if data, err := resource.List(); err != nil {
+					errTplt.Execute(w, err)
+				} else if err := listTemplate.Execute(w, data); err != nil {
+					common.Error.Printf("Rendering failed: %s", err)
+				} else {
+					common.Trace.Printf("%q", data)
+				}
+			case "new":
+				newTemplate.Execute(w, struct{}{})
+			default:
+				if id, err := strconv.Atoi(url); err != nil {
+					errTplt.Execute(w, common.New400Error("Invalid %s id '%s'", name, url))
+				} else if data, err := resource.Get(id); err != nil {
+					errTplt.Execute(w, err)
+				} else if err := getTemplate.Execute(w, data); err != nil {
+					common.Error.Printf("Rendering failed: %s", err)
+				} else {
+					common.Trace.Printf("%q", data)
+				}
+			}
+		default:
+			errTplt.Execute(w, common.New404Error("Resource does not exist"))
+		}
+	}
+
+	mux.Handle(common.Url(name, ""), http.StripPrefix(common.Url(name, ""), handler))
 }
 
 var Mux = &serveMux{http.NewServeMux()}
--- common/errors.go
+++ common/errors.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -10,18 +11,18 @@ type HTTPError struct {
 	Msg        string `json:"payload"`
 }
 
-func NewHTTPError(msg string, code int) *HTTPError {
-	return &HTTPError{code, http.StatusText(code), msg}
+func NewHTTPError(code int, msg string, i ...interface{}) *HTTPError {
+	return &HTTPError{code, http.StatusText(code), fmt.Sprintf(msg, i...)}
 }
 
-func New404Error(msg string) *HTTPError {
-	return NewHTTPError(msg, http.StatusNotFound)
+func New404Error(msg string, i ...interface{}) *HTTPError {
+	return NewHTTPError(http.StatusNotFound, msg, i...)
 }
 
-func New500Error(msg string) *HTTPError {
-	return NewHTTPError(msg, http.StatusInternalServerError)
+func New500Error(msg string, i ...interface{}) *HTTPError {
+	return NewHTTPError(http.StatusInternalServerError, msg, i...)
 }
 
-func New400Error(msg string) *HTTPError {
-	return NewHTTPError(msg, http.StatusBadRequest)
+func New400Error(msg string, i ...interface{}) *HTTPError {
+	return NewHTTPError(http.StatusBadRequest, msg, i...)
 }
